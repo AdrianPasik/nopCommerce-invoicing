bfc4282b1c52d60a6862e4170d5b6bd612d53249
Adrian Pasik <adrian@nonexistendmail.com>
2019-12-09 22:43:22 +0100

    restructuring, new code in node serving

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const fs = require('fs')
 const app = express()
 const port = 3000
 
-app.get('/faktura', (req, res) => {
-    res.sendFile('templates/sales/sales.html', { root: __dirname });
+app.get('/faktura', (req, response) => {
+    fs.readFile("templates/sales/sales.html", function (err, data) {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(data);
+        response.end();
+     });
+    //res.sendFile('templates/sales/sales.html', { root: __dirname });
     //res.send('Hello World!');
 });
 
