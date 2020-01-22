4328c406f92bd74d2803802b65daddfff0e09450
Adrian Pasik <adrian.pasik@vp.pl>
2020-01-23 00:02:21 +0100

    begining to parse date

--- src/csv/parse-nop-csv.js
+++ src/csv/parse-nop-csv.js
@@ -1,25 +1,61 @@
 const parse = require('csv-parse/lib/sync')
 const fs = require('fs');
 
-var contents = fs.readFileSync('C:\\praca\\orders.csv', 'utf8');
-
-
 class ParseNopCsv {
-	static getInvoicesSync(csvContent) {
-		const records = parse(contents, {
-			delimiter: ","
-		  })
+	static getInvoicesSync(orderCSV) {
+		const orderRecords = this.parseCSV(orderCSV);
 
-		if (records.lenght == 0 || records.lenght == 1) {
+		if (orderRecords.lenght == 0 || orderRecords.lenght == 1) {
 			return { };
 		}
 
 
-		return records;
+
+		let orderCollection = [];
+
+		orderCollection.push(this.getOneEntity(orderRecords, 0));
+
+		return orderRecords;
+	}
+
+	static parseCSV(rawCSV) {
+		return parse(rawCSV, {
+			delimiter: ","
+		  });
+	}
+
+	static getOneEntity(orderRecordsArray, indexOfOrder) {
+		// take order
+		let entity = {};
+		let titleRowFound = false;
+		for(var i = indexOfOrder; i < orderRecordsArray.length - 1; i++) {
+			if(orderRecordsArray[i][0] == "OrderId") {
+				if(!titleRowFound) {
+					titleRowFound = true;
+					continue;
+				} else {
+					break;
+				}
+			}
+			// z is on 26th place in excel file
+			entity["sell-date"] = this.fromOADate(orderRecordsArray[i][29]);
+			// take order details
+		}
+		
+	}
+
+	static fromOADate(dateAsFloat) {
+		var date = new Date(((dateAsFloat - 25569) * 86400000));
+		var tz = date.getTimezoneOffset();
+		return new Date(((dateAsFloat - 25569 + (tz / (60 * 24))) * 86400000));
 	}
 }
 
-var records = ParseNopCsv.getInvoicesSync(contents);
+var ordersCsv = fs.readFileSync('C:\\praca\\orders.csv', 'utf8');
+
+var x = ParseNopCsv.fromOADate(Number("43797.5599752083"));
+
+var records = ParseNopCsv.getInvoicesSync(ordersCsv);
 
 module.exports = ParseNopCsv;
 
